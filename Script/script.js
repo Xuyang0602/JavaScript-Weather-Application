98f7a222311c51eae7f345581ace6e17df8a5499
xuyangwang0827 <xuyangwang0827@gmail.com>
2018-09-13 12:54:38 -0700

    Weather & GEO API and Get Weather Forecast

--- Script/script.js
+++ Script/script.js
@@ -1,4 +1,13 @@
 /**
+ *      Init
+ *      
+ */
+
+window.onload = () => {
+    UI.showApp();
+};
+
+/**
  *      UI Elements Module
  *      
  *      - this module will be responsible for controling UI 
@@ -56,11 +65,90 @@
     };
  })();
 
-/**
- *      Init
+ /**
+ *      Get Location Module
  *      
+ *      - this module will be responsible for getting the data
+ *        about the location to search for weather
  */
 
- window.onload = () => {
-     UI.showApp();
- }
+const GETLOCATION = (function() {
+    let location;
+    let locationIput = document.querySelector("#location-input");
+    let addCityBtn = document.querySelector("#add-city-btn");
+
+    locationIput.addEventListener('input', function() {
+        let inputValue = this.value.trim();
+
+        if (inputValue != '') {
+            addCityBtn.removeAttribute('disabled');
+            addCityBtn.classList.remove('disabled');
+        } else {
+            addCityBtn.setAttribute('disabled', 'true');
+            addCityBtn.classList.add('disabled');
+        }
+    });
+
+    const _addCity = () => {
+        location = locationIput.value;
+        locationIput.value = "";
+        addCityBtn.setAttribute('disabled', 'true');
+        addCityBtn.classList.add('disabled');
+
+        // Get Weather Data
+        WEATHER.getWeather(location);
+    };
+
+    addCityBtn.addEventListener('click', _addCity);
+})();
+
+ /**
+ *      Get Weather Data
+ *      
+ *      - this module will aquire weather data and then it 
+ *        will pass to another module which will put the 
+ *        data on UI
+ */
+
+ const WEATHER = (function() {
+     const darkSkyKey = 'abb6c4bc746ff07ddc578a3ce70710d5'; 
+     const geoCodeKey = '9e3c900b3f2a492f9ec243a042480958';
+
+     const _getDarkSkyURL = (lat, lng) => `https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/${darkSkyKey}/${lat},${lng}`;
+     const _getGeocodeURL = (location) => `https://api.opencagedata.com/geocode/v1/json?q=${location}&key=${geoCodeKey}`;
+
+     const _getDarkSkyDate = (url) => {
+         axios.get(url)
+            .then( (res) => {
+                console.log(res);
+            }).catch( (err) => {
+                console.log(err);
+            });
+     }
+
+     const getWeather = (location) => {
+         UI.loadApp();
+
+         let geocodeURL = _getGeocodeURL(location);
+
+         console.log(geocodeURL);
+
+         axios.get(geocodeURL)
+            .then( (res) => {
+                // console.log(res.data.results[0].geometry);
+                let lat = res.data.results[0].geometry.lat;
+                let lng = res.data.results[0].geometry.lng;
+
+                let darkskyURL = _getDarkSkyURL(lat, lng);
+                _getDarkSkyDate(darkskyURL);
+
+            }).catch( (err) => {
+                console.log(err);
+            });
+     };
+
+     return {
+        getWeather
+     };
+
+ })();
